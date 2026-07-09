cb429b6390820fbe683861d8ee956d281ea0a917
lmoussafir <leonassim@hotmail.fr>
2026-07-09 10:24:02 +0900

    Add rhps1_mj_description and point rl_controller to rhps1 branch
    - rl_controller now tracks origin/rhps1 (RHPS1 walking policy port:
      PostureTask + KinematicsConstraint, V2 observation, correct timing)
    - Add rhps1_mj_description from isri-aist/rhps1_mj_description

--- extensions/controllers/rl_controller.cmake
+++ extensions/controllers/rl_controller.cmake
@@ -1,5 +1,5 @@
 AddProject(rl_controller
   GITHUB bastien-muraccioli/new-rl-qp-controller
-  GIT_TAG origin/main
+  GIT_TAG origin/rhps1
   DEPENDS mc_rtc
 )
--- robots/RHPS1.cmake
+++ robots/RHPS1.cmake
@@ -18,3 +18,10 @@ AddProject(
   GIT_TAG origin/master
   DEPENDS rhps1_description mc_rtc
 )
+
+AddProject(
+  rhps1_mj_description
+  GITHUB_PRIVATE isri-aist/rhps1_mj_description
+  GIT_TAG origin/master
+  DEPENDS mc_rtc
+)
