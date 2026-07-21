f099f150f202bf5fe890841dd38ef3592b933cae
lmoussafir <leonassim@hotmail.fr>
2026-07-22 00:06:13 +0900

    fix(mc_residual_estimation): use main, devel needs an mc_rtc feature not in any available branch

--- extensions/plugins/mc_residual_estimation.cmake
+++ extensions/plugins/mc_residual_estimation.cmake
@@ -1,5 +1,10 @@
 AddProject(mc_residual_estimation
   GITHUB bastien-muraccioli/mc_residual_estimation
-  GIT_TAG origin/devel
+  # main (was devel, 2026-07-22, Léo): devel's ExternalForcesEstimator.h
+  # requires mc_rbdyn/VirtualTorqueSensor.h, which doesn't exist in any
+  # currently available mc_rtc branch (bastien's master/hrp5p, or upstream
+  # jrl-umi3218 master) -- devel is ahead of what mc_rtc can build against
+  # right now. main's ExternalForcesEstimator.h doesn't need it.
+  GIT_TAG origin/main
   DEPENDS mc_rtc
 )
